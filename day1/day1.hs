fcae3f3fc8d2487687f7410cf28de05050ad8932
Harshit <harsjoshi1614@gmail.com>
2023-12-01 14:49:39 +0530

    Refactored day1 code

--- day1/day1.hs
+++ day1/day1.hs
@@ -2,22 +2,10 @@ import Data.Map.Strict (Map, delete, fromList, lookup, member, notMember, size, 
 
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
--- numbers = map show [0..9]
---  Can we make [0..9] into a String "0123"
-
--- we need a custom drop function that either parses or drops chars
-
-parseItem :: String -> String
-parseItem = dropWhile (`notElem` numbers)
-
-toInt :: String -> Int
-toInt str = read [head str, last str] :: Int
-
 fullParse :: String -> Int
-fullParse str = toInt $ reverse $ parseItem $ reverse $ parseItem str
-
-part1 :: [Int] -> Int
-part1 = sum
+fullParse str = do
+  let f_str = filter (`elem` numbers) str
+  read [head f_str, last f_str] :: Int
 
 -- Part 2
 
@@ -33,16 +21,6 @@ match ('s' : 'i' : 'x' : xs) = Just 6
 match ('s' : 'e' : 'v' : 'e' : 'n' : xs) = Just 7
 match ('e' : 'i' : 'g' : 'h' : 't' : xs) = Just 8
 match ('n' : 'i' : 'n' : 'e' : xs) = Just 9
-match ('o' : 'r' : 'e' : 'z' : xs) = Just 0
-match ('e' : 'n' : 'o' : xs) = Just 1
-match ('o' : 'w' : 't' : xs) = Just 2
-match ('e' : 'e' : 'r' : 'h' : 't' : xs) = Just 3
-match ('r' : 'u' : 'o' : 'f' : xs) = Just 4
-match ('e' : 'v' : 'i' : 'f' : xs) = Just 5
-match ('x' : 'i' : 's' : xs) = Just 6
-match ('n' : 'e' : 'v' : 'e' : 's' : xs) = Just 7
-match ('t' : 'h' : 'g' : 'i' : 'e' : xs) = Just 8
-match ('e' : 'n' : 'i' : 'n' : xs) = Just 9
 match ('0' : xs) = Just 0
 match ('1' : xs) = Just 1
 match ('2' : xs) = Just 2
@@ -53,8 +31,19 @@ match ('6' : xs) = Just 6
 match ('7' : xs) = Just 7
 match ('8' : xs) = Just 8
 match ('9' : xs) = Just 9
+match ('o' : 'r' : 'e' : 'z' : xs) = Just 0
+match ('e' : 'n' : 'o' : xs) = Just 1
+match ('o' : 'w' : 't' : xs) = Just 2
+match ('e' : 'e' : 'r' : 'h' : 't' : xs) = Just 3
+match ('r' : 'u' : 'o' : 'f' : xs) = Just 4
+match ('e' : 'v' : 'i' : 'f' : xs) = Just 5
+match ('x' : 'i' : 's' : xs) = Just 6
+match ('n' : 'e' : 'v' : 'e' : 's' : xs) = Just 7
+match ('t' : 'h' : 'g' : 'i' : 'e' : xs) = Just 8
+match ('e' : 'n' : 'i' : 'n' : xs) = Just 9
 match _ = Nothing
 
+-- tried to `match` recursively to input String
 try :: String -> String
 try [] = error "Length too Short !"
 try str = do
@@ -62,6 +51,7 @@ try str = do
     Just val -> show val
     Nothing -> try $ tail str
 
+-- parses twice once for front digit, once for the last digit (By reversing the string)
 fullParse2 :: String -> Int
 fullParse2 str = do
   let d1 = try str
@@ -72,8 +62,9 @@ main :: IO ()
 main = do
   bytes <- readFile "input.txt"
   let lns = lines bytes
-  let nums = map fullParse lns
-  print $ part1 nums
 
-  let p2 = map fullParse2 lns
-  print $ sum p2
+  let p1 = sum $ map fullParse lns
+  print $ "Part 1 -> " ++ show p1
+
+  let p2 = sum $ map fullParse2 lns
+  print $ "Part 2 -> " ++ show p2
