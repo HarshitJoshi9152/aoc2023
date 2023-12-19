0b06a31ba96d450c56e9c536323f2a710613fa85
Harshit <harsjoshi1614@gmail.com>
2023-12-19 16:38:32 +0530

    Day 6 in 0(1)
    thanks to @riscord for explaining me the quad equation

--- day6/day6.hs
+++ day6/day6.hs
@@ -1,33 +1,47 @@
 import Data.Char (isDigit)
 import Data.List.Split (splitOn)
 
-works :: Int -> Int -> Int -> Bool
-works dist baseTime remainingTime = dist < remainingTime * speed
+quadraticEquation :: (Integral b, Integral p1, Integral p2) => p1 -> p2 -> b
+quadraticEquation d t = ceiling $ (-b + sqrt ((b ** 2) - (4 * a * c))) / (2 * a)
   where
-    speed = baseTime - remainingTime
+    a = -1
+    b = fromIntegral t
+    c = fromIntegral (-d)
 
-waysp2 :: (Int, Int) -> Int
-waysp2 (time, dist) = count
+-- Equation here =>
+--    dist = speed * time left
+--    d = x * (y - x)
+--    d = x * (t - x)
+--    d = tx - x^2
+--    x^2 - tx + d || -x^2 + tx -d
+--    Coefficients =>
+--      a = -1
+--      b = t
+--      c = -d
+--    By completing the squares we get
+--
+--    -b +- sqrt(b^2 - 4ac)
+--    --------------------
+--             2a
+
+ways :: (Int, Int) -> Int
+ways (time, dist) = count
   where
-    stx = head $ dropWhile (not . works dist time) [1 .. (time - 1)]
+    -- stx = head $ dropWhile (not . works dist time) [1 .. (time - 1)] -- Dumb me
+    stx = quadraticEquation dist time
     end = time - stx
     count = end - stx + 1
 
-ways :: (Int, Int) -> Int
-ways (time, dist) = length $ filter id $ map (works dist time) [1 .. (time - 1)]
-
--- Calculate for [1..(time-1)]
 main :: IO ()
 main = do
   input <- readFile "./input.txt"
   let lns = lines input
   -- Part 1 Solution
   let [times, dists] :: [[Int]] = map (map read . filter (not . null) . splitOn " " . dropWhile (not . isDigit)) lns
-  let p1 = zipWith (curry waysp2) times dists
+  let p1 = zipWith (curry ways) times dists
   print $ "Part 1 -> " ++ (show . product) p1
 
   -- Part 2 Solution
   let [time, dist] :: [Int] = map (read . filter isDigit) lns
-  let p2 = waysp2 (time, dist)
-
+  let p2 = ways (time, dist)
   print $ "Part 2 -> " ++ show p2
