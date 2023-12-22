1545a3f0e0e230b489dbbb94ebc387f1512024b3
Harshit <harsjoshi1614@gmail.com>
2023-12-22 16:09:28 +0530

    Day 7 Part 2 !

--- day7/day7.hs
+++ day7/day7.hs
@@ -1,10 +1,9 @@
 import Data.Char (isSpace, isAlphaNum)
 import Data.List (sort, group)
 
-data Card = ONE | TWO | THREE | FOUR | FIVE | SIX | SEVEN | EIGHT | NINE | T | J | Q | K | A deriving (Eq, Ord);
+data Card = J | TWO | THREE | FOUR | FIVE | SIX | SEVEN | EIGHT | NINE | T | Q | K | A deriving (Eq, Enum, Bounded, Ord);
 
 instance Show Card where
-    show ONE = "1"
     show TWO = "2"
     show THREE = "3"
     show FOUR = "4"
@@ -23,7 +22,6 @@ instance Read Card where
     readsPrec _ c =
         let (s, r) = span (/= ' ') c
         in case c of
-             "1" -> [(ONE, r)]
              "2" -> [(TWO, r)]
              "3" -> [(THREE, r)]
              "4" -> [(FOUR, r)]
@@ -39,7 +37,7 @@ instance Read Card where
              "A" -> [(A, r)]
              _ -> error "no parse"
 
-data Order = HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind deriving (Show, Read, Eq, Ord);
+data Order = HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind deriving (Enum, Bounded, Show, Read, Eq, Ord);
 
 data Deck = Deck {
     cards :: (Card, Card, Card, Card, Card),
@@ -49,7 +47,7 @@ data Deck = Deck {
 
 instance Ord Deck where
     (Deck cards o _) `compare` (Deck cards' o' _) =
-        if o == o' then 
+        if o == o' then
             cards `compare` cards'
         else
             o `compare` o'
@@ -68,21 +66,51 @@ toCard str =
 toCards :: String -> [Card]
 toCards str = [ toCard [c] | c <- str, isAlphaNum c]
 
+-- freqToOrder :: [Int] -> Order
+-- freqToOrder [5] = FiveOfAKind
+-- freqToOrder [4, 1] = FourOfAKind
+-- freqToOrder [3, 2] = FullHouse
+-- freqToOrder [3, 1, 1] = ThreeOfAKind
+-- freqToOrder [2, 2, 1] = TwoPair
+-- freqToOrder [2, 1, 1, 1] = OnePair
+-- freqToOrder [1, 1, 1, 1, 1] = HighCard
+-- freqToOrder _ = error "Not 5 cards"
 
+-- for debugging
 freqToOrder :: [Int] -> Order
-freqToOrder [5] = FiveOfAKind
-freqToOrder [4, 1] = FourOfAKind
-freqToOrder [3, 2] = FullHouse
-freqToOrder [3, 1, 1] = ThreeOfAKind
-freqToOrder [2, 2, 1] = TwoPair
-freqToOrder [2, 1, 1, 1] = OnePair
-freqToOrder [1, 1, 1, 1, 1] = HighCard
-freqToOrder _ = error "Not 5 cards 🤨"
+freqToOrder arr
+    | arr == [5] = FiveOfAKind
+    | arr == [4, 1] = FourOfAKind
+    | arr == [3, 2] = FullHouse
+    | arr == [3, 1, 1] = ThreeOfAKind
+    | arr == [2, 2, 1] = TwoPair
+    | arr == [2, 1, 1, 1] = OnePair
+    | arr == [1, 1, 1, 1, 1] = HighCard
+    | otherwise = error ("Not 5 cards" ++ show arr)
+
+mostCommon :: Ord a => [a] -> a
+mostCommon = snd . maximum . map (\xs -> (length xs, head xs)) . group . sort
+
+promoteCard :: [Card] -> [Card]
+promoteCard [J, J, J, J, J] = [J, J, J, J, J]
+promoteCard cards = r
+    where
+        l = length $ filter (==J) cards
+        m = mostCommon cards
+        -- For cases like xxJJJ
+        m' = case m of 
+            J -> mostCommon $ filter (/=J) cards -- take the next mostCommon After J ! (BUT WHAT IF ITS ALL J ?)
+            _ -> m
+        rest = [c | c <- cards, c /= J]
+        r = replicate l m' ++  rest
+
 
 orderCards :: [Card] -> Order
-orderCards cards = freqToOrder $ reverse $ sort freq
+orderCards cards = freqToOrder freq'
     where
-        freq = map length $ group $ sort cards
+        -- freq = map length $ group $ sort cards -- Part 1 solution
+        freq = map length $ group $ sort $ promoteCard cards -- Part 2 solution
+        freq' = reverse $ sort freq
 
 parse :: String -> Deck
 parse ln = Deck ( toTuple cards ) order score
@@ -90,11 +118,11 @@ parse ln = Deck ( toTuple cards ) order score
         w = words ln
         score = read $ last w
         cards :: [Card] = toCards $ head w
-        order :: Order = orderCards cards 
+        order :: Order = orderCards cards
 
 winnings :: [Deck] -> Int
 winnings decks = sum d
-    where 
+    where
         d = zipWith (\a b -> b * score a) decks [1..]
 
 main :: IO ()
@@ -104,7 +132,5 @@ main = do
 
     let decks :: [Deck] = map parse lns
 
-    -- print decks
-    -- print $ sort decks
     print $ winnings $ sort decks
 
